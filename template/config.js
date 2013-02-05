70fdf5d1f2f6063d169887a70955408117601e9b
Cameron Bytheway <cameron@nujii.com>
2013-02-04 18:40:39 -0700

    Use hubot style app deployment

--- template/config.js
+++ template/config.js
@@ -3,22 +3,25 @@
 //------------------------------------
 
 // MongoDB connection URI for the app
-exports.db_uri = "mongodb://localhost/strider-foss";
+exports.db_uri = process.env.MONGODB_URI ||
+                 process.env.MONGOHQ_URL ||
+                 process.env.MONGOLAB_URL ||
+                 "mongodb://localhost/strider-foss";
 
 // Session store secret
-exports.session_secret = "8L8BudMkqBUqrz";
+exports.session_secret = process.env.SESSION_SECRET || "this_is_a_secret";
 
 // Server port
-exports.server_port = 3000;
+exports.server_port = process.env.PORT || 3000;
 
 // Server URL on the Internet
-var sm = exports.strider_server_name = "http://localhost:3000";
+var sm = exports.strider_server_name = process.env.SERVER_NAME || "http://localhost:" + exports.server_port;
 
 // Github OAuth2 API config
 // Register your own at: https://github.com/settings/applications/new
 exports.github = {
-    appId: "a3af4568e9d8ca4165fe",
-    appSecret: "18651128b57787a3336094e2ba1af240dfe44f6c",
+    appId: process.env.GITHUB_APP_ID || "a3af4568e9d8ca4165fe",
+    appSecret: process.env.GITHUB_APP_SECRET || "18651128b57787a3336094e2ba1af240dfe44f6c",
     // Re-use strider_server_name
     myHostname: sm
 };
@@ -28,11 +31,9 @@ exports.github = {
 exports.zeromq_addr = "tcp://0.0.0.0:31012";
 
 // Email settings
-exports.smtp = {
-    host: "smtp.foo.com",
-    port: 587,
-    username: "foobar",
-    password: "setme"
+exports.sendgrid = {
+    username: process.env.SENDGRID_USERNAME || "user"
+  , password: process.env.SENDGRID_PASSWORD || "pass"
 };
 
 // Logging configuration
@@ -48,4 +49,3 @@ exports.logging = {
   },
   console_enabled: true
 }
-
