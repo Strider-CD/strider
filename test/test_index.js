986269e0d7afbf1db43e8667120f9aabfa782351
Niall O'Higgins <niallo@unworkable.org>
2013-04-09 16:11:08 -0700

    fix tests for manual setup case.
    closes #66

--- test/test_index.js
+++ test/test_index.js
@@ -29,7 +29,7 @@ describe('index', function() {
       var response_api = {end: function() {}};
       var mock_user = {github:{id:"123"}, github_metadata:{"123":{repos:[]}}};
       var mock_payload = {"ref":"refs/heads/master"}
-      var mock_repo = {has_prod_deploy_target: false, collaborators:[]};
+      var mock_repo = {has_prod_deploy_target: false, collaborators:[], display_url:"https://github.com/foo/bar"};
       var jobs_api = {startJob: function(user, repo, deploy_config, repo_ssh_url) {}};
       common.emitter.once('queue.new_task', function(data) {
         zmq_api.new_test_task(data.user,
@@ -44,7 +44,9 @@ describe('index', function() {
             callback(true, mock_repo, mock_user, mock_payload);
         },
         webhook_commit_is_to_master:gh.webhook_commit_is_to_master,
-        webhook_extract_latest_commit_info:function() {}
+        webhook_extract_latest_commit_info:function() {},
+        make_ssh_url:gh.make_ssh_url,
+        parse_github_url:gh.parse_github_url
       };
       var index = sandboxed_module.require('../routes', {
         requires: {'../lib/jobs':jobs_api, '../lib/github':gh_api, '../lib/common':common }
