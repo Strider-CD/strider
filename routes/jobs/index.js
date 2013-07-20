0eb812179fafc1a55f05c577431938baeee7f93b
Jared Forsyth <jared@jaredforsyth.com>
2013-07-20 03:07:49 -0600

    starting to mess with things

--- routes/jobs/index.js
+++ routes/jobs/index.js
@@ -38,10 +38,10 @@ function lookup(case_insensitive_url, cb) {
 };
 
 /*
- * GET /org/repo/latest_build - view latest build for repo
+ * GET /org/repo/[jobid] - view latest build for repo
  */
 
-exports.latest_build = function(req, res)
+exports.job = function(req, res)
 {
   res.statusCode = 200;
   var org = req.params.org;
@@ -54,64 +54,15 @@ exports.latest_build = function(req, res)
       res.end("you must configure " + repo_url + " before you can use it");
       return;
     }
-    Job.find()
-        .sort({'finished_timestamp': -1})
-        .where('finished_timestamp').ne(null)
-        .where('archived_timestamp', null)
-        .where('repo_url', repo_config.url)
-        .where('type').in(['TEST_ONLY','TEST_AND_DEPLOY'])
-        .limit(20)
-        .lean(true)
-        .populate("_owner")
-        .exec(function(err,results)
-    {
-      if (err) throw err;
+    var saferepo = utils.sanitizeRepo(repo_config);
 
-      _.each(results, function(job) {
-        job.duration = Math.round((job.finished_timestamp - job.created_timestamp)/1000);
-        job.finished_at = humane.humaneDate(job.finished_timestamp);
-        job.triggered_by_commit = false;
-        if (job.github_commit_info !== undefined && job.github_commit_info.id !== undefined) {
-          job.triggered_by_commit = true;
-          job.gravatar_url = 'https://secure.gravatar.com/avatar/' + crypto.createHash('md5').update(job.github_commit_info.author.email).digest("hex") + '.jpg?' + 'd=' + encodeURIComponent('identicon');
-
-
-          if (job.github_commit_info.author.username != undefined) {
-            job.committer = job.github_commit_info.author.username;
-            job.committer_is_username = true;
-          } else {
-            job.committer = job.github_commit_info.author.name;
-            job.committer_is_username = false;
-          }
-        }
-        job.id = job._id.toString();
-        job.url = "/" + org + "/" + repo + "/job/" + job.id;
-      });
-
-      if (results.length === 0) {
-        return res.end('no jobs for this build');
-      }
-      var triggered_by_commit = false;
-      if (results[0].github_commit_info !== undefined && results[0].github_commit_info.id !== undefined) {
-        triggered_by_commit = true;
-      }
-
-      var job = utils.sanitizeJob(results[0]);
-      var saferepo = utils.sanitizeRepo(repo_config);
-
-      res.render('latest_build.html',
-        {
-          admin_view: false,
-          jobs: results,
-          results_detail: results[0],
-          job: job,
-          triggered_by_commit: triggered_by_commit,
-          org:org,
-          repo_config: saferepo,
-          repo:repo,
-          repo_url:repo_config.url,
-          has_prod_deploy_target:repo_config.has_prod_deploy_target
-        });
+    res.render('build.html', {
+      admin_view: false,
+      org:org,
+      repo:repo,
+      repo_config: saferepo,
+      repo_url: repo_config.url,
+      has_prod_deploy_target: repo_config.has_prod_deploy_target
     });
   });
 };
@@ -162,6 +113,7 @@ exports.badge = function(req, res) {
  * index.job - build the job detail page
  */
 
+/**
 exports.job = function(req, res)
 {
   res.statusCode = 200;
@@ -195,7 +147,6 @@ exports.job = function(req, res)
       console.log("Querying for last 20 jobs for " + repo_url);
       Job.find()
         .sort({'finished_timestamp': -1})
-        .where('finished_timestamp').ne(null)
         .where('repo_url',this.repo_config.url)
         .where('archived_timestamp', null)
         .where('type').in(['TEST_ONLY','TEST_AND_DEPLOY'])
@@ -206,6 +157,8 @@ exports.job = function(req, res)
     },
     function processAndRender(err, results_detail, results) {
       if (err) throw err;
+      // if results_detail did not return, that means this is not a valid job id
+      if (!results_detail) return res.render(404, 'invalid job id');
 
       _.each(results, function(job) {
         job.id = job._id.toString();
@@ -232,52 +185,47 @@ exports.job = function(req, res)
         job.url = "/" + org + "/" + repo + "/job/" + job.id;
       });
 
-      // if results_detail did not return, that means this is not a valid job id
-      if (!results_detail) {
-        res.render(404, 'invalid job id');
-      } else {
 
-        results_detail.duration = Math.round((results_detail.finished_timestamp - results_detail.created_timestamp)/1000);
-        results_detail.finished_at = humane.humaneDate(results_detail.finished_timestamp);
-        results_detail.id = results_detail._id.toString();
+      results_detail.duration = Math.round((results_detail.finished_timestamp - results_detail.created_timestamp)/1000);
+      results_detail.finished_at = humane.humaneDate(results_detail.finished_timestamp);
+      results_detail.id = results_detail._id.toString();
 
-        var triggered_by_commit = false;
-        if (results_detail.github_commit_info !== undefined && results_detail.github_commit_info.id !== undefined) {
-          triggered_by_commit = true;
-          results_detail.gravatar_url = 'https://secure.gravatar.com/avatar/' + crypto.createHash('md5').update(results_detail.github_commit_info.author.email).digest("hex") + '.jpg?' + 'd=' + encodeURIComponent('identicon');
-          if (results_detail.github_commit_info.author.username != undefined) {
-            results_detail.committer = results_detail.github_commit_info.author.username;
-            results_detail.committer_is_username = true;
-          } else {
-            results_detail.committer = results_detail.github_commit_info.author.name;
-            results_detail.committer_is_username = false;
-          }
-        }
-
-        // Jobs which have not finished have no output
-        if (!results_detail.stdmerged) {
-          results_detail.output = "[STRIDER] This job has no output."
+      var triggered_by_commit = false;
+      if (results_detail.github_commit_info !== undefined && results_detail.github_commit_info.id !== undefined) {
+        triggered_by_commit = true;
+        results_detail.gravatar_url = 'https://secure.gravatar.com/avatar/' + crypto.createHash('md5').update(results_detail.github_commit_info.author.email).digest("hex") + '.jpg?' + 'd=' + encodeURIComponent('identicon');
+        if (results_detail.github_commit_info.author.username != undefined) {
+          results_detail.committer = results_detail.github_commit_info.author.username;
+          results_detail.committer_is_username = true;
         } else {
-          results_detail.output = filter(results_detail.stdmerged);
+          results_detail.committer = results_detail.github_commit_info.author.name;
+          results_detail.committer_is_username = false;
         }
-
-        var job = utils.sanitizeJob(results_detail);
-        job.triggered_by_commit = triggered_by_commit;
-        res.render('job.html',
-          {
-            admin_view: false,
-            jobs: results,
-            job: job,
-            repo_config: utils.sanitizeRepo(this.repo_config),
-            results_detail: results_detail,
-            job_id:results[0].id.substr(0,8),
-            triggered_by_commit: triggered_by_commit,
-            org:org,
-            repo:repo,
-            repo_url:this.repo_config.url,
-            has_prod_deploy_target:this.repo_config.has_prod_deploy_target
-          });
       }
+
+      // Jobs which have not finished have no output
+      if (!results_detail.stdmerged) {
+        results_detail.output = "[STRIDER] This job has no output."
+      } else {
+        results_detail.output = filter(results_detail.stdmerged);
+      }
+
+      var job = utils.sanitizeJob(results_detail);
+      job.triggered_by_commit = triggered_by_commit;
+      res.render('job.html', {
+        admin_view: false,
+        jobs: results,
+        job: job,
+        repo_config: utils.sanitizeRepo(this.repo_config),
+        results_detail: results_detail,
+        job_id:results_detail.id.substr(0,8),
+        triggered_by_commit: triggered_by_commit,
+        org:org,
+        repo:repo,
+        repo_url:this.repo_config.url,
+        has_prod_deploy_target:this.repo_config.has_prod_deploy_target
+      });
     }
   );
 };
+***/
