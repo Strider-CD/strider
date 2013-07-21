5ef3f9af7124bb5856a1e5e9068aaa3ca3404620
Jared Forsyth <jared@jaredforsyth.com>
2013-07-20 20:37:59 -0600

    option to strip out ansi stuff

--- test/test_ansi.js
+++ test/test_ansi.js
@@ -13,4 +13,7 @@ describe('ansi filtering', function () {
   it('should work w/ mocha output', function () {
     expect(filter(loadFix('1.in'))).to.equal(loadFix('1.out'));
   });
+  it('should strip correctly', function () {
+    expect(filter(loadFix('2.in'), true)).to.equal(loadFix('2.out'));
+  });
 })
