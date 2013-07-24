a97356c069c731fe3bac2955eeda912a9a4faa0e
Jared Forsyth <jared@jaredforsyth.com>
2013-07-23 20:02:15 -0600

    adding more ansi tests

--- test/test_ansi.js
+++ test/test_ansi.js
@@ -11,9 +11,17 @@ function loadFix(name) {
 
 describe('ansi filtering', function () {
   it('should work w/ mocha output', function () {
-    expect(filter(loadFix('1.in'))).to.equal(loadFix('1.out'));
-  });
-  it('should strip correctly', function () {
-    expect(filter(loadFix('2.in'), true)).to.equal(loadFix('2.out'));
-  });
+    expect(filter(loadFix('1.in'))).to.equal(loadFix('1.out'))
+  })
+  describe('with plaintext option', function () {
+    it('should strip correctly', function () {
+      expect(filter(loadFix('2.in'), true)).to.equal(loadFix('2.out'))
+    })
+  })
+  it('should preserve leading CR', function () {
+    expect(filter('somethen\relse')).to.equal('\relse')
+  })
+  it("should not add leading cr when it's CRLF", function () {
+    expect(filter('something\r\nelse')).to.equal('something\r\nelse')
+  })
 })
