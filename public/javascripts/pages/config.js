d7de26f828335b30bd4c1090fa73874ffb7e86c8
Jared Forsyth <jared@jaredforsyth.com>
2013-07-30 16:13:56 -0600

    floating, fading message

--- public/javascripts/pages/config.js
+++ public/javascripts/pages/config.js
@@ -10,29 +10,57 @@ app.controller('Config', ['$scope', '$element', function ($scope, $element, $att
   $scope.repo = JSON.parse($element.attr('data-repo-config') || '{}');
   $scope.panelData = JSON.parse($element.attr('data-panel-data') || '{}');
   $scope.gravatar = function (email) {
+    if (!email) return '';
     var hash = md5(email.toLowerCase());
     return 'https://secure.gravatar.com/avatar/' + hash + '?d=identicon';
   }
   $scope.error = function (text) {
     $scope.message = {
       text: text,
-      type: 'error'
+      type: 'error',
+      showing: true
     };
   };
   $scope.info = function (text) {
     $scope.message = {
       text: text,
-      type: 'info'
+      type: 'info',
+      showing: true
     };
   };
-  $scope.success = function (text) {
+  var waitTime = null;
+  $scope.success = function (text, sticky) {
+    if (waitTime) {
+      clearTimeout(waitTime);
+      waitTime = null;
+    }
+    if (clearTime) {
+      clearTimeout(clearTime);
+      clearTime = null;
+    }
     $scope.message = {
       text: '<strong>Done.</strong> ' + text,
-      type: 'success'
+      type: 'success',
+      showing: true
     };
+    if (!sticky) {
+      waitTime = setTimeout(function () {
+        $scope.clearMessage();
+        $scope.$digest();
+      }, 5000);
+    }
   };
+  var clearTime = null;
   $scope.clearMessage = function () {
-    $scope.message = null;
+    if (clearTime) {
+      clearTimeout(clearTime);
+    }
+    $scope.message.showing = false;
+    clearTime = setTimeout(function () {
+      clearTime = null;
+      $scope.message = null;
+      $scope.$digest();
+    }, 1000);
   };
 }]);
 
