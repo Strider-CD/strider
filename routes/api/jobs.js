5639d6470b9f2120bea516fdfe74fae76e1f6404
Jared Forsyth <jared@jaredforsyth.com>
2013-09-25 18:20:57 -0600

    DB upgrading works, dashboard working, config page relatively functional, build page inital

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -17,7 +17,6 @@ var _ = require('underscore')
   , Job = require(BASE_PATH + 'models').Job
   , User = require(BASE_PATH + 'models').User
   , logging = require(BASE_PATH + 'logging')
-  ;
 
 var TEST_ONLY = "TEST_ONLY";
 var TEST_AND_DEPLOY = "TEST_AND_DEPLOY";
@@ -63,6 +62,9 @@ exports.jobs_start = function(req, res) {
  * GET /api/jobs
  * Return JSON object containing the most recent build status for each configured repo
  * This function is used to build the main dashboard status page
+ * The result is separated into {public: [], yours: []}.
+ *
+ * Note: the private ones are just ones that the current user is a collaborator on. Not necessarily private
  */
 exports.jobs = function(req, res) {
 
@@ -70,37 +72,7 @@ exports.jobs = function(req, res) {
   res.setHeader("Content-Type", "application/json");
   res.statusCode = 200;
 
-  var query
-  if (!req.user || !req.user.projects) {
-    // TODO allow anonymous view for public projects
-    return res.send(401, 'Anonymous access not allowed')
-  }
-  var projects = Object.keys(req.user.projects)
-    , tasks = []
-  projects.forEach(function (project) {
-    tasks.push(function (next) {
-      Job.find({project: project})
-         .sort({finished: -1})
-         .limit(6)
-         .lean().exec(function (err, jobs) {
-           next(err, {
-             name: project,
-             latest: jobs && jobs[0],
-             previous: jobs && jobs.slice(1)
-           })
-         })
-    })
+  jobs.latestJobs(req.user, function (err, jobs) {
+    res.send(JSON.stringify(jobs, null, '\t'))
   })
-  async.parallel(tasks, function (err, projects) {
-    if (err) return res.send(500, 'Failed to get most recent jobs')
-    projects.sort(function(a, b) {
-      if (!a.latest) return -1
-      if (!b.latest) return 1
-      if (!a.latest.finished || !a.latest.finished.getTime) return -1
-      if (!b.latest.finished || !b.latest.finished.getTime) return 1
-      return b.latest.finished.getTime() - a.latest.finished.getTime()
-    });
-    var output = JSON.stringify(projects, null, '\t');
-    res.end(output);
-  });
 }
