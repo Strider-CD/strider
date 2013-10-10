130a6c238860c7da9d353031d7637918e791a197
Peter Braden <peterbraden@peterbraden.co.uk>
2013-10-10 11:22:57 -0700

    link instead of subdir in dev script

--- dev_1_4.sh
+++ dev_1_4.sh
@@ -18,10 +18,10 @@ for module in $DEPS
 do
   if [ "$1" = "up" ]
   then
-    (cd node_modules/$module && git pull && rm -rf node_modules && npm i)
+    (../$module && git pull && rm -rf node_modules && npm i)
   else
-    git clone -b 1_4_refactor $BASE/$module node_modules/$module
-    (cd node_modules/$module && npm install)
+    git clone -b 1_4_refactor $BASE/$module ../$module
+    npm link ../$module
   fi
 done
 
