4057b5e599c2fb40c912adc3b771ffc5420142d2
Peter Braden <peterbraden@peterbraden.co.uk>
2013-10-15 12:09:27 -0700

    WIP on integration tests

--- test/integration/login_test.js
+++ test/integration/login_test.js
@@ -4,40 +4,7 @@ var sm = require('mocha-selenium')
       appCmd: 'make serve-test'
     })
 
-
-var _prev = null
-var flow = function(){
-  var _arguments = Array.prototype.slice.call(arguments)
-    , cmd = _arguments.shift()
-
-  if (typeof(cmd) == "function"){
-    return cmd(null, _prev)
-  }
-
-  var method = cmd[0]
-    , args = []
-
-  for (var i = 1; i< cmd.length; i++){
-    if (cmd[i] == '$')
-      args.push(_prev)
-    else
-      args.push(cmd[i]);
-  }
-
-  //console.log(">", method, ":", args)
-  args.push(function(err, res){
-    if (err) throw err;
-    _prev = res
-    return flow.apply(this, _arguments);
-  })
-  b[method].apply(b, args)
-}
-
-
-
-
-
-
+var test = it; // Tests as english sucks. 'it' doesn't even make sens for half of these.
 
 describe('login', function(){
   this.timeout(30 * 1000)
@@ -47,19 +14,19 @@ describe('login', function(){
   })
 
 
-  it("should render the homepage", function(done){
+  test("render the homepage", function(done){
     b.visibleByCss("a.brand", function(err){
       done(err)
     })
   })
 
-  it("should render the login form", function(done){
+  test("render the login form", function(done){
     b.visibleByCss("#navbar-signin-form", function(err){
       done(err)
     })
   })
 
-  it("submitting bad creds fails", function(done){
+  test("submitting bad creds fails", function(done){
     b.chain()
      .rel('/')
      .fillInForm({
@@ -77,7 +44,7 @@ describe('login', function(){
      })
   })
 
-  it("follow forgot password flow", function(done){
+  test("follow forgot password flow", function(done){
     b.chain()
      .rel("/")
      .elementById("forgot-password-link", function(err, el){
@@ -99,7 +66,7 @@ describe('login', function(){
 
   })
 
-  it("submitting form works", function(done){
+  test("submitting form works", function(done){
     b.chain()
      .rel('/')
      .fillInForm({
