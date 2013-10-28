e4d9ab165b4fa381e7fb367120b93cd7f7958b3e
Niall O'Higgins <niallo@frozenridge.co>
2013-10-28 15:32:53 -0700

    don't override process.env.DB_URI

--- test/strider.js
+++ test/strider.js
@@ -6,7 +6,6 @@
 
 require('./setup-fixtures')(function(err, config) {
   console.log(config)
-  process.env.DB_URI = config.db_uri
   var child = require('child_process').spawn("bin/strider", ["--config", 'test/test-config.json'], { 
     env : process.env,
     stdio: 'inherit',
