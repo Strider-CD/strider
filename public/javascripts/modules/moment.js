d42cdb34c1cc2f03df613da0a09b2b0177cc6658
Jared Forsyth <jared@jaredforsyth.com>
2013-11-03 01:42:28 -0700

    bugfixes

--- public/javascripts/modules/moment.js
+++ public/javascripts/modules/moment.js
@@ -124,4 +124,10 @@ app.directive("time", function() {
       });
     }
   };
+}).filter('percentage', function () {
+  return function (input, prec) {
+    if (!input && parseInt(input) !== 0) return '';
+    var by = Math.pow(10, prec || 1)
+    return parseInt(parseFloat(input) * by, 10)/by + '%'
+  }
 });
