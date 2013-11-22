71ccc3200e1385982e9dc5f3ad18c19031922883
Niall O'Higgins <niallo@frozenridge.co>
2013-11-21 16:36:44 -0800

    docker start script

---
+++ docker/start-strider.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+env
+MONGODB_URI=mongodb://localhost/strider-foss
+if [ "$MONGO_PORT" != ""]; then
+  MONGODB_URI="mongodb://${MONGO_PORT#tcp://}/strider-foss"
+fi
+DB_URI=$MONGODB_URI
+echo "DB_URI: $MONGODB_URI"
+DB_URI=$DB_URI node bin/strider
