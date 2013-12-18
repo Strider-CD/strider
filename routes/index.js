7ef1dbb6d27ec231bbc27426c5a84ac999f29b58
Jared Forsyth <jared@jaredforsyth.com>
2013-12-17 21:43:16 -0700

    moving slashes so that it doesn't break plugin assets

--- routes/index.js
+++ routes/index.js
@@ -254,6 +254,11 @@ exports.config = function(req, res) {
 
     var provider = common.extensions.provider[req.project.provider.id]
       , creator_creds = req.project.creator.account(req.project.provider).config
+    if (!provider) {
+      // TODO: alert the user through the UI
+      console.warn('Provider plugin not installed!', req.project.provider.id)
+      return respond(data)
+    }
     if (typeof provider.getBranches === 'function' && creator_creds) {
       provider.getBranches(creator_creds,
         req.project.provider.config, req.project, function(err, branches) {
