38d621ecd91783167087c7831370035ec30b82b0
Adam Duncan <adam.duncan@clock.co.uk>
2013-12-20 16:24:17 +0000

    Add tests for findBranch()

--- test/unit/test_utils.js
+++ test/unit/test_utils.js
@@ -5,7 +5,9 @@ var _ = require('underscore')
   , path = require('path')
 
 describe('utils', function () {
-  describe('.defaultSchema', function () {
+
+  describe('defaultSchema()', function () {
+
     it('should cover a complex case', function () {
       expect(utils.defaultSchema({
         runtime: {
@@ -19,9 +21,11 @@ describe('utils', function () {
         test: 'npm test'
       })
     })
+
   })
 
-  describe('validateAgainstSchema', function () {
+  describe('validateAgainstSchema()', function () {
+
     it('should cover a complex case', function () {
       expect(utils.validateAgainstSchema({
         unknown: 'key',
@@ -46,9 +50,11 @@ describe('utils', function () {
         }
       })
     })
+
   })
 
-  describe('.mergePlugins', function () {
+  describe('mergePlugins()', function () {
+
     it('should work for nulls', function () {
       expect(utils.mergePlugins(null, null)).to.equal(null)
       expect(utils.mergePlugins([], null)).to.eql([])
@@ -83,6 +89,103 @@ describe('utils', function () {
         {id: 'two', config: {two: 2}}
       ])
     })
+
   })
-});
+
+  describe('findBranch()', function () {
+
+    var branches =
+      [ { name: 'test' }
+      , { name: 'test*test' }
+      , { name: 'test*' }
+      , { name: '*test' }
+      , { name: '*test*' }
+      , { name: '*' }
+      ]
+
+    it('should find a branch with no wildcards', function () {
+      var branch = utils.findBranch(branches, 'test')
+      expect(branch.name).to.equal('test')
+    })
+
+    it('should find a branch with wildcard at end', function () {
+      var branch = utils.findBranch(branches, 'test*')
+      expect(branch.name).to.equal('test*')
+    })
+
+    it('should find a branch with wildcard at start', function () {
+      var branch = utils.findBranch(branches, '*test')
+      expect(branch.name).to.equal('*test')
+    })
+
+    it('should find a branch with wildcard at start and end', function () {
+      var branch = utils.findBranch(branches, '*test*')
+      expect(branch.name).to.equal('*test*')
+    })
+
+    it('should find a branch with wildcard in middle', function () {
+      var branch = utils.findBranch(branches, 'test*test')
+      expect(branch.name).to.equal('test*test')
+    })
+
+    it('should find a branch with only wildcard', function () {
+      var branch = utils.findBranch(branches, '*')
+      expect(branch.name).to.equal('*')
+    })
+
+    it('should match wildcard in middle branch', function () {
+      var branch = utils.findBranch(branches, 'testingtest')
+      expect(branch.name).to.equal('test*test')
+    })
+
+    it('should match wildcard at end branch', function () {
+      var branch = utils.findBranch(branches, 'testing')
+      expect(branch.name).to.equal('test*')
+    })
+
+    it('should match wildcard at start branch', function () {
+      var branch = utils.findBranch(branches, 'mytest')
+      expect(branch.name).to.equal('*test')
+    })
+
+    it('should match wildcard at start and end branch', function () {
+      var branch = utils.findBranch(branches, 'mytestingbranch')
+      expect(branch.name).to.equal('*test*')
+    })
+
+    it('should match wildcard only branch', function () {
+      var branch = utils.findBranch(branches, 'branch')
+      expect(branch.name).to.equal('*')
+    })
+
+  })
+
+})
+
+
+
+
+// function test(string, against) {
+//   var regEx = new RegExp('^' + string.replace(/\*/g, '.*') + '$')
+//   return regEx.test(against)
+// }
+
+// console.log('true:')
+// console.log('* : master', test('*', 'master'))
+// console.log('master : master', test('master', 'master'))
+// console.log('master* : master', test('master*', 'master'))
+// console.log('*master : master', test('*master', 'master'))
+// console.log('master* : mastered', test('master*', 'mastered'))
+// console.log('*master : testmaster', test('*master', 'testmaster'))
+// console.log('*master* : testmastered', test('*master*', 'testmastered'))
+// console.log('feature/* : feature/my-new-feature', test('feature/*', 'feature/my-new-feature'))
+// console.log('feature/* : feature/', test('feature/*', 'feature/'))
+// console.log('feature/* : feature/', test('feature/*', 'feature/'))
+
+// console.log('false:')
+// console.log('master* : test', test('master*', 'test'))
+// console.log('*master : test', test('*master', 'test'))
+// console.log('master* : test', test('master*', 'test'))
+// console.log('feature/ : feature/my-new-feature', test('feature/', 'feature/my-new-feature'))
+// console.log('test : testing', test('test', 'testing'))
 
