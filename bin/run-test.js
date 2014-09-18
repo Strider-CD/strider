dba055ddffe0eeb334a2879be1ec04c533dab171
Ilya Radchenko <ilya@burstcreations.com>
2014-09-18 11:53:48 -0400

    Refactor run-test
    Move run to own module, and cleanup

--- bin/run-test.js
+++ bin/run-test.js
@@ -1,54 +1,10 @@
 'use strict';
 
+var Step = require('step');
+var readline = require('readline');
+var run = require('./run');
+
 function runTest(email, password, project, branch, message, deploy) {
-  var Step = require('step');
-  var readline = require('readline');
-  var request = require('superagent');
-
-  var run = function() {
-    var agent = request.agent()
-    var server_name = require('../lib/config').server_name
-    var url = server_name + "/api/session"
-    request.post(url)
-      .send({ email: email, password: password })
-      .end(function(err, res){
-        if(!err && res.statusCode !== 404){
-          agent.saveCookies(res);
-          url = server_name + "/"+project+"/start"
-          var req = request.post(url),
-          postData = {branch: branch || "master"};
-
-          agent.attachCookies(req);
-
-          if (message)
-              postData.message = message;
-
-          if(deploy)
-              postData.type = 'TEST_AND_DEPLOY';
-
-          req.send(postData);
-
-          req.end(function(err, res){
-            if(!err && res.statusCode !== 404){
-              console.log("Job started")
-            }
-            else if(res.statusCode === 404){
-              console.log("Error: Repo was not found")
-            }
-            else{
-              console.log("Error: ", err);
-            }
-            process.exit(0);
-          })
-
-        }
-        else{
-          console.log("Login error")
-          process.exit(0);
-        }
-    });
-  }
-
   if(!email || !password || !project){
     var rl = readline.createInterface({
       input: process.stdin,
@@ -56,45 +12,55 @@ function runTest(email, password, project, branch, message, deploy) {
     });
 
     Step(
-      function getEmail(){
-          var next = this;
-          if (email){
-            next();
-          } else {
-            rl.question("Enter email []: ", function(em){
-              email = em;
-              next();
-            });
-          }
+      function getEmail() {
+        var next = this;
+
+        if (email){
+          next();
         }
-      , function getPwd(){
-          var next = this;
-          if (password){
+        else {
+          rl.question('Enter email []: ', function (em) {
+            email = em;
             next();
-          } else {
-            rl.question("Enter password []: ", function(pwd){
-              password = pwd;
-              next();
-            });
-          }
+          });
         }
-      , function getProject(){
-          var next = this;
-          if (project){
+      },
+
+      function getPwd() {
+        var next = this;
+
+        if (password){
+          next();
+        } else {
+          rl.question('Enter password []: ', function (pwd) {
+            password = pwd;
             next();
-          } else {
-            rl.question("Project name []: ", function(pr){
-              project = pr;
-              next();
-            });
-          }
+          });
+        }
+      },
+
+      function getProject() {
+        var next = this;
+
+        if (project){
+          next();
+        } else {
+          rl.question('Project name []: ', function (pr) {
+            project = pr;
+            next();
+          });
+        }
+      },
+      
+      function runTest() {
+        run();
       }
-      , run
-      );
+    );
   }
-  else{
+  else {
     run();
   }
 }
+
 
 module.exports = runTest;
