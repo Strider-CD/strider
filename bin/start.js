b7213bce173ec9f99dcd716d9855e5afd4c120e7
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-23 17:16:16 -0700

    extract plugin path code from start() into a lib

--- bin/start.js
+++ bin/start.js
@@ -1,31 +1,14 @@
 'use strict';
 
+var common = require('../lib/common');
+var path = require('path');
+var main = require('../main');
+var getPluginPath = require('../lib/plugin_path');
+
 function start(extpath) {
-  var common = require('../lib/common');
-  var path = require('path');
-  var main = require('../main');
-  var extpaths = require('../lib/config').extpath.split(':');
-  var extdir = [];
-
-  for (var i in extpaths) {
-    // Extensions are either in ../node_modules (if local)
-    // or __dirname/../
-    extdir.push(path.resolve(__dirname + "../" + extpaths[i]));
-
-    try {
-      fs.statSync(extdir);
-    } catch(e) {
-      extdir.pop();
-      extdir.push(path.resolve(__dirname + "/../" + extpaths[i]));
-    }
-  }
-
-  if (extpath){
-    extdir.push(path.resolve(extpath));
-  }
-
+  var extdir = getPluginPath(extpath);
   // Save extension dir
-  common.extdir = extdir;
+  common.extdir = extdir
   main(extdir);
 }
 
