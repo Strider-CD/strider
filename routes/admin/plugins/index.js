1d77f4e21dcb426f1d679a7b83bf7fcbbac72a15
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-16 09:11:44 -0700

    require path earlier

--- routes/admin/plugins/index.js
+++ routes/admin/plugins/index.js
@@ -25,7 +25,7 @@ module.exports = function(req, res, next) {
     localPlugins.listAll(function(err, localPlugins) {
       localPlugins.forEach(function(plugin) {
         if (! plugins[plugin.name]) {
-          var pkg = require(require('path').join(plugin.path, 'package.json'))
+          var pkg = require(path.join(plugin.path, 'package.json'))
           plugins[plugin.name] = {
             description: pkg.description,
             type: pkg.strider.type,
