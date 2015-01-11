d203219ed24f8e0ab23289425250ae092de2c651
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-01-10 23:22:03 -0500

    update "dev.sh" script for use on ubuntu
    add flags:
            * --sudo-link to use sudo when using npm link
            * --ignore-link-fail to ignore when npm link fails

--- dev.sh
+++ dev.sh
@@ -5,22 +5,38 @@
 
 # you can hack on them by editing under node_moduldes/strider-foo
 
+ARGS=$@
+case "${ARGS[@]}" in *"--sudo-link"*) SUDOLINK="sudo" ;; esac
+case "${ARGS[@]}" in *"--ignore-link-fail"*) IGNORELINKFAIL="YES" ;; esac
+
 npm install lodash
 DEPS=$(node -e "var _ = require('lodash'), deps = _.keys(require('./package').dependencies); console.log(_.filter(deps, function(i){ return /^strider-/.test(i) }).join(' '));")
 
-BASE="git@github.com:/Strider-CD"
+BASE="git@github.com:Strider-CD"
 
 mkdir -p node_modules
 
+
 for module in $DEPS
 do
-  if [ "$1" = "up" ]
-  then
-    (cd ../$module && git pull && rm -rf node_modules && npm i)
-  else
+  if [ ! -d ../$module ]; then
     git clone $BASE/$module ../$module
-    (cd ../$module && npm install)
-    npm link ../$module
+  fi
+  pushd ../$module
+  npm i
+  popd
+  if [ ! -L $PWD/node_modules/$module ]; then
+    $SUDOLINK npm link ../$module
+    if [ $? != 0 ]; then
+      echo "Failed to link $module"
+      if [ -z $SUDOLINK ]; then
+        echo "You may need to use root for npm link, pass in --sudo-link to run the command with sudo."
+      fi
+      if [ -z $IGNORELINKFAIL ]; then
+        echo "If you want to ignore link failures, pass in --ignore-link-fail"
+        exit -1
+      fi
+    fi
   fi
 done
 
