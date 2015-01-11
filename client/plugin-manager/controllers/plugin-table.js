189d35a1cf341965a8aa1a4fe70b5b89af4616a2
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-01-11 01:09:24 -0800

    implement "upgrade all" in plugin manager
    closes #671

--- client/plugin-manager/controllers/plugin-table.js
+++ client/plugin-manager/controllers/plugin-table.js
@@ -1,18 +1,43 @@
 'use strict';
 
-module.exports = function($http, $timeout) {
+module.exports = function($scope) {
   this.busy = false;
 
-  this.hasUpgrades = function() {
+  this.hasUpgrades = (function() {
     for (var name in plugins) {
       var plugin = plugins[name];
       if (plugin.outdated) return true;
     }
-    return $('.autoupgrade').length > 0;
-  }
+    return false;
+  }());
 
   this.upgradeAll = function() {
-    $('.autoupgrade').click();
+    this.busy = true;
+    var ticks = 0;
+    var list = [];
+    for (var name in plugins) {
+      var plugin = plugins[name];
+      if (plugin.outdated) list.push(plugin);
+    }
+
+    var upgrader = function(i){
+      if ( i < list.length ) {
+        var pluginCtrl = list[i].controller;
+        pluginCtrl.upgrade(function(err) {
+          if (err) {
+            return global.alert("Batch upgrade aborted due to error:\n"+err.message);
+          }
+          ++ticks;
+          upgrader(i+1);
+          if (ticks === list.length) {
+            this.busy = false;
+            this.hasUpgrades = false;
+          }
+        }.bind(this));
+      }
+    }.bind(this);
+
+    upgrader(0);
   }
 
   this.uninstall = function(plugin) {
@@ -39,7 +64,11 @@ module.exports = function($http, $timeout) {
 
   this.upgrade = function(plugin) {
     this.busy = true;
-    plugin.upgrade(function() {
+    plugin.upgrade(function(err) {
+      if (err) {
+        global.alert(err.message);
+      }
+
       this.busy = false;
     }.bind(this))
   }
--- client/plugin-manager/index.js
+++ client/plugin-manager/index.js
@@ -9,6 +9,6 @@ var interpolate = require('../utils/interpolate');
 var app = angular.module('plugin-manager', [])
   .config(['$interpolateProvider', interpolate])
   .controller('PluginController', ['$http', '$timeout', PluginController])
-  .controller('PluginTableController', [PluginTableController]);
+  .controller('PluginTableController', ['$scope', PluginTableController]);
 
 module.exports = app;
