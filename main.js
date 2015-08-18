14a17cbe4c3b663bdb7ba5f00d79ce98fd5dd5ea
Ilya Radchenko <ilya@burstcreations.com>
2015-08-17 20:23:16 -0400

    Move functions from main to utils

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 var path = require('path');
 var passport = require('passport');
 var async = require('async');
-var _ = require('lodash');
 var Loader = require('strider-extension-loader');
 var globalTunnel = require('global-tunnel');
 
@@ -14,11 +13,10 @@ var middleware = require('./lib/middleware');
 var auth = require('./lib/auth');
 var models = require('./lib/models');
 var pluginTemplates = require('./lib/plugin-templates');
-var utils = require('./lib/utils');
-var apiConfig = require('./lib/routes/api/config');
 var upgrade = require('./lib/models/upgrade').ensure;
 var loadExtensions = require('./lib/utils/load-extensions');
-var pkg = require('./package');
+var killZombies = require('./lib/utils/kill-zombies');
+var registerPanel = require('./lib/utils/register-panel');
 
 var Job = models.Job;
 var Config = models.Config;
@@ -29,30 +27,8 @@ common.extensions = {};
 // The http_proxy environment variable will be used if the first parameter to globalTunnel.initialize is null.
 //
 globalTunnel.initialize();
-//
-// ### Register panel
-//
-// A panel is simply a snippet of HTML associated with a given key.
-// Strider will output panels registered for specific template.
-//
-function registerPanel(key, value) {
-  // Nothing yet registered for this panel
-  key = value.id;
-  console.log('!! registerPanel', key)
 
-  if (common.extensions[key] === undefined) {
-    common.extensions[key] = { panel: value };
-  } else {
-    if (common.extensions[key].panel) {
-      console.log('!!', key, common.extensions[key], value);
-      throw 'Multiple Panels for ' + key;
-    }
-
-    common.extensions[key].panel = value;
-  }
-}
-
-module.exports = function(extdir, c, callback) {
+module.exports = function (extdir, c, callback) {
   var appConfig = config;
   var k;
   // override with c
@@ -62,9 +38,11 @@ module.exports = function(extdir, c, callback) {
 
   // Initialize the (web) app
   var appInstance = app.init(appConfig);
-  var cb = callback || function(err) {
-    if (err) throw err;
-  }
+  var cb = callback || function (err) {
+    if (err) {
+      throw err;
+    }
+  };
 
   if (typeof Loader !== 'function') {
     throw new Error('Your version of strider-extension-loader is out of date');
@@ -94,7 +72,7 @@ module.exports = function(extdir, c, callback) {
     middleware: middleware,
     auth: auth, //TODO - may want to make this a subset of the auth module
     passport: passport,
-    registerPanel: registerPanel,
+    registerPanel: registerPanel(common),
     registerBlock: pluginTemplates.registerBlock,
     app: appInstance
   };
@@ -105,14 +83,16 @@ module.exports = function(extdir, c, callback) {
   var SCHEMA_VERSION = Config.SCHEMA_VERSION;
 
   upgrade(SCHEMA_VERSION, function (err) {
-    if (err) return cb(err)
+    if (err) {
+      return cb(err);
+    }
 
     loadExtensions(loader, extdir, context, appInstance, function (err) {
       // kill zombie jobs
       killZombies(function () {
         var tasks = [];
 
-        if (!common.extensions.runner || 'object' !== typeof common.extensions.runner) {
+        if (!common.extensions.runner || typeof common.extensions.runner !== 'object') {
           console.error('Strider seems to have been misconfigured - there are no available runner plugins. ' +
             'Please make sure all dependencies are up to date.');
           process.exit(1);
@@ -130,14 +110,14 @@ module.exports = function(extdir, c, callback) {
             Job.find({
               'runner.id': name,
               finished: null
-            }, function (err, jobs) {
-              if (err) {
-                return next(err);
+            }, function (error, jobs) {
+              if (error) {
+                return next(error);
               }
 
               runner.findZombies(jobs, next);
             });
-          })
+          });
         });
 
         async.parallel(tasks, function (err, zombies) {
@@ -157,18 +137,4 @@ module.exports = function(extdir, c, callback) {
   });
 
   return appInstance;
-}
-
-function killZombies(done) {
-  console.log('Marking zombie jobs as finished...');
-
-  Job.update({archived: null, finished: null},
-    {$set: {finished: new Date(), errored: true}}, {multi: true}, 
-    function(err, count) {
-      if (err) throw err;
-      console.log('%d zombie jobs marked as finished', count);
-      done();
-    }
-  );
-}
-
+};
