4194de854a89cdcdd494aaf66d719f8322a7a326
nomean42 <ygreknomean@gmail.com>
2016-01-27 12:39:31 +0600

    Add trigger for pullrequest icon

--- client/job-status/controllers/job.js
+++ client/job-status/controllers/job.js
@@ -108,7 +108,7 @@ module.exports = function ($scope, $route, $location, $filter) {
   });
 
   $scope.triggers = {
-    pullrequst: {
+    pullrequest: {
       icon: 'exchange',
       title: 'Pullrequest'
     },
