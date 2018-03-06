d26499754912b67a07c385f8d10821e13024a425
Tayaa Med Amine <tayamino@users.noreply.github.com>
2018-03-06 01:47:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ bin/strider install gitlab
 bin/strider install heroku
 
 bin/strider addUser -a true -l $ACL_ADMIN_PSEUDO -p $ACL_ADMIN_PASSWD -f
+
+npm install strider-{email-notifier,status-report,metadata,mail,slack,custom,python,ruby}
+npm install strider-{docker-{build,slave},remote-worker,ssh-deploy,{simple,docker,digitalocean}-runner}
