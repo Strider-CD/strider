b552fde7838ca31187692fa2e2fc04d32a5faf70
Tayaa Med Amine <tayamino@users.noreply.github.com>
2018-03-06 01:35:55 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ bin/strider install github
 bin/strider install bitbucket
 bin/strider install gitlab
 bin/strider install heroku
+
+bin/strider addUser -a true -l $ACL_ADMIN_PSEUDO -p $ACL_ADMIN_PASSWD -f
