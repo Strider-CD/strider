0a31ce79941241a66a19b08c58450796f6cb6b92
Ilya Radchenko <knownasilya@gmail.com>
2020-04-27 22:25:13 -0400

    feat(job): add cancel and fix status (#1088)
    Cancel existed before, but I didn't add it in the first round for the Ember client. Also fixed some status things. Still a bit more to go here.

--- client-ember/app/pods/organization/repository/-components/job/component.ts
+++ client-ember/app/pods/organization/repository/-components/job/component.ts
@@ -1,7 +1,22 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import io from 'socket.io-client';
 
 interface Args {
   job: object;
 }
 
-export default class Job extends Component<Args> {}
+export default class Job extends Component<Args> {
+  socket: SocketIOClient.Socket;
+
+  constructor(owner: unknown, args: Args) {
+    super(owner, args);
+    let socket = io.connect();
+    this.socket = socket;
+  }
+
+  @action
+  cancel(jobId: string) {
+    this.socket.emit('cancel', jobId);
+  }
+}
