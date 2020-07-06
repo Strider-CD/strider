e581706ba037c3048381e6343aebd040c409aa92
Ilya Radchenko <knownasilya@gmail.com>
2020-07-06 08:37:44 -0400

    fix: update types

--- clients/strider-ui/tests/integration/helpers/ansi-test.js
+++ clients/strider-ui/tests/integration/helpers/ansi-test.js
@@ -1,13 +1,13 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Helper | ansi', function(hooks) {
+module('Integration | Helper | ansi', function (hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     this.set('inputValue', '1234');
 
     await render(hbs`{{ansi inputValue}}`);
--- clients/strider-ui/tests/integration/helpers/format-date-test.ts
+++ clients/strider-ui/tests/integration/helpers/format-date-test.ts
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Helper | format-date', function (hooks) {
   setupRenderingTest(hooks);
