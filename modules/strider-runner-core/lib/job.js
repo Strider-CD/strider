d0658e2974aac4b916005c23fdd7b8bc689036a0
Ilya Radchenko <knownasilya@gmail.com>
2020-07-10 00:21:44 -0400

    fix: runner code on phase.errored (#1099)
    * fix: pass correct code to phase.errored
    
    * chore: update repo url

--- modules/strider-runner-core/lib/job.js
+++ modules/strider-runner-core/lib/job.js
@@ -39,13 +39,18 @@ module.exports = Job;
 function Job(job, provider, plugins, config) {
   var jobId = job._id.toString();
 
-  debug(`Creating new job "${jobId}" for project "${job.project.display_name}" using provider "${provider.id}".`);
+  debug(
+    `Creating new job "${jobId}" for project "${job.project.display_name}" using provider "${provider.id}".`
+  );
 
-  this.config = _.extend({
-    spawn: spawnNormal,
-    pty: false,
-    env: {}
-  }, config);
+  this.config = _.extend(
+    {
+      spawn: spawnNormal,
+      pty: false,
+      env: {},
+    },
+    config
+  );
 
   // Setup default ENVs to be used in plugin contexts
   this.config.env = _.extend(this.config.env, {
@@ -56,7 +61,7 @@ function Job(job, provider, plugins, config) {
     STRIDER_BRANCH: job.ref.branch,
     STRIDER_SLUG: job.project.name,
     STRIDER_BUILD: jobId,
-    STRIDER_BUILD_URL: `${job.project.display_url}/job/${jobId}`
+    STRIDER_BUILD_URL: `${job.project.display_url}/job/${jobId}`,
   });
 
   if (this.config.pty) {
@@ -87,13 +92,17 @@ Job.prototype = {
 
     // no plugins in use, nothing will happen. fail.
     if (!this.plugins.length) {
-      return this.done({message: 'No plugins configured. Aborting'}, done);
+      return this.done({ message: 'No plugins configured. Aborting' }, done);
     }
 
     // If the plugin has a listen function, execute it
     this.plugins.forEach(function (plugin) {
       if (plugin.listen) {
-        var context = self.pluginContext(self.provider.id, _.cloneDeep(self.config.env), []);
+        var context = self.pluginContext(
+          self.provider.id,
+          _.cloneDeep(self.config.env),
+          []
+        );
         plugin.listen(self.io, context);
       }
     });
@@ -167,7 +176,7 @@ Job.prototype = {
     this.status('command.comment', {
       comment: text,
       plugin: plugin,
-      time: new Date()
+      time: new Date(),
     });
   },
 
@@ -198,98 +207,111 @@ Job.prototype = {
     var self = this;
     var start = new Date();
     var options = {
-      cwd: this.phase === 'environment' ? this.config.baseDir : this.config.dataDir,
+      cwd:
+        this.phase === 'environment'
+          ? this.config.baseDir
+          : this.config.dataDir,
       silent: false,
-      detached: process.platform !== 'win32'
+      detached: process.platform !== 'win32',
     };
     var std = {
       out: '',
-      err: ''
+      err: '',
     };
 
     cmd = utils.normalizeCmd(cmd, options);
 
-    this.config.spawn(cmd.command, cmd.args, options, function (err, proc) {
-      if (err) {
-        if (err === 127) {
-          return next(127, '', 'Command not found');
+    this.config.spawn(
+      cmd.command,
+      cmd.args,
+      options,
+      function (err, proc) {
+        if (err) {
+          if (err === 127) {
+            return next(127, '', 'Command not found');
+          }
+
+          debug('fail!', err, cmd.command, cmd.args);
+          return next(500, '', `Unable to execude command :( ${err}`);
         }
 
-        debug('fail!', err, cmd.command, cmd.args);
-        return next(500, '', `Unable to execude command :( ${err}`);
-      }
+        proc.stdout.setEncoding('utf8');
+        proc.stderr.setEncoding('utf8');
 
-      proc.stdout.setEncoding('utf8');
-      proc.stderr.setEncoding('utf8');
+        proc.stdout.on('data', function (buf) {
+          if (!options.silent) {
+            self.status('stdout', buf);
+          }
 
-      proc.stdout.on('data', function (buf) {
-        if (!options.silent) {
-          self.status('stdout', buf);
-        }
-
-        std.out += buf;
-      });
-
-      proc.stderr.on('data', function (buf) {
-        if (!options.silent) {
-          self.status('stderr', buf);
-        }
-
-        std.err += buf;
-      });
-
-      proc.on('exit', function (exitCode) {
-        var end = new Date();
-        var elapsed = end.getTime() - start.getTime();
-
-        // Uncomment to monitor job commands
-        // self.log(util.format('command done %s %s; exit code %s; duration %s',
-        // cmd.command, cmd.args, exitCode, elapsed))
-        self.status('command.done', {
-          exitCode: exitCode,
-          time: end,
-          elapsed: elapsed
+          std.out += buf;
         });
 
-        var ind = -1;
-
-        for (var i = 0; i < self.commands.length; i++) {
-          if (self.commands[i].proc === proc) {
-            ind = i;
-            break;
+        proc.stderr.on('data', function (buf) {
+          if (!options.silent) {
+            self.status('stderr', buf);
           }
-        }
 
-        if (ind !== -1) {
-          self.commands.splice(ind, 1);
-        }
+          std.err += buf;
+        });
 
-        next(exitCode, std.out, std.err);
-      });
+        proc.on('exit', function (exitCode) {
+          var end = new Date();
+          var elapsed = end.getTime() - start.getTime();
 
-      proc.on('error', function () {
-        // prevent node from throwing an error
-      });
+          // Uncomment to monitor job commands
+          // self.log(util.format('command done %s %s; exit code %s; duration %s',
+          // cmd.command, cmd.args, exitCode, elapsed))
+          self.status('command.done', {
+            exitCode: exitCode,
+            time: end,
+            elapsed: elapsed,
+          });
 
-      var strCmd = shellQuote([cmd.command].concat(cmd.args));
-      var display = cmd.screen || strCmd;
+          var ind = -1;
 
-      this.status('command.start', {command: display, started: start, plugin: plugin, time: start});
+          for (var i = 0; i < self.commands.length; i++) {
+            if (self.commands[i].proc === proc) {
+              ind = i;
+              break;
+            }
+          }
 
-      if (options.silent) {
-        this.status('stdout', '[output suppressed]');
-      }
+          if (ind !== -1) {
+            self.commands.splice(ind, 1);
+          }
 
-      this.commands.push({
-        cmd: {
+          next(exitCode, std.out, std.err);
+        });
+
+        proc.on('error', function () {
+          // prevent node from throwing an error
+        });
+
+        var strCmd = shellQuote([cmd.command].concat(cmd.args));
+        var display = cmd.screen || strCmd;
+
+        this.status('command.start', {
           command: display,
           started: start,
           plugin: plugin,
-          time: start
-        },
-        proc: proc
-      });
-    }.bind(this));
+          time: start,
+        });
+
+        if (options.silent) {
+          this.status('stdout', '[output suppressed]');
+        }
+
+        this.commands.push({
+          cmd: {
+            command: display,
+            started: start,
+            plugin: plugin,
+            time: start,
+          },
+          proc: proc,
+        });
+      }.bind(this)
+    );
   },
 
   // job running stuff
@@ -304,17 +326,23 @@ Job.prototype = {
       },
 
       cmd: function (cmd, next) {
-        if (typeof(cmd) === 'string' || cmd.command) {
-          cmd = {cmd: cmd};
+        if (typeof cmd === 'string' || cmd.command) {
+          cmd = { cmd: cmd };
         }
 
-        cmd.env = _.extend({}, env, {
-          PATH: getPath(path),
-          HOME: process.env.HOME || (process.env.HOMEDRIVE + process.env.HOMEPATH),
-          LANG: process.env.LANG,
-          SSH_AUTH_SOCK: process.env.SSH_AUTH_SOCK,
-          PAAS_NAME: 'strider'
-        }, cmd.env || {});
+        cmd.env = _.extend(
+          {},
+          env,
+          {
+            PATH: getPath(path),
+            HOME:
+              process.env.HOME || process.env.HOMEDRIVE + process.env.HOMEPATH,
+            LANG: process.env.LANG,
+            SSH_AUTH_SOCK: process.env.SSH_AUTH_SOCK,
+            PAAS_NAME: 'strider',
+          },
+          cmd.env || {}
+        );
 
         return self.cmd(cmd, pluginName, next);
       },
@@ -325,7 +353,7 @@ Job.prototype = {
           plugin: pluginName,
           data: data,
           method: method,
-          path: path
+          path: path,
         });
       },
 
@@ -341,11 +369,11 @@ Job.prototype = {
       branch: this.job.ref.branch,
       project: this.project,
 
-      runnerId: (
-        this.config.branchConfig
-        && this.config.branchConfig.runner
-        && this.config.branchConfig.runner.id || 'unkown'
-      )
+      runnerId:
+        (this.config.branchConfig &&
+          this.config.branchConfig.runner &&
+          this.config.branchConfig.runner.id) ||
+        'unkown',
     };
 
     return context;
@@ -366,28 +394,44 @@ Job.prototype = {
 
     // need to clone the repo first
     if (this.phase === 'prepare') {
-      tasks.push(this.provider.fetch.bind(this.provider, self.pluginContext(this.provider.id, env, path)));
+      tasks.push(
+        this.provider.fetch.bind(
+          this.provider,
+          self.pluginContext(this.provider.id, env, path)
+        )
+      );
     }
 
     this.plugins.forEach(function (plugin) {
       debug('Running plugin', plugin.id);
       // all plugins will get the final, fully populated env object
-      _.extend(env, ('function' === typeof plugin.env) ? plugin.env(self.phase) : plugin.env);
+      _.extend(
+        env,
+        'function' === typeof plugin.env ? plugin.env(self.phase) : plugin.env
+      );
 
       if ('string' === typeof plugin.path) {
         path.push(plugin.path);
       } else {
-        path = path.concat('function' === typeof plugin.path ? plugin.path(self.phase) : plugin.path);
+        path = path.concat(
+          'function' === typeof plugin.path
+            ? plugin.path(self.phase)
+            : plugin.path
+        );
       }
 
       tasks.push(function (next) {
-        utils.runPlugin(self.phase, plugin, self.pluginContext(plugin.id, env, path), next);
+        utils.runPlugin(
+          self.phase,
+          plugin,
+          self.pluginContext(plugin.id, env, path),
+          next
+        );
       });
     });
 
     async.series(tasks, this.phaseDone.bind(this, next, started));
   },
-
 
   // called on the completion (or erroring) of a phase
   phaseDone: function (next, started, err, actuallyRan) {
@@ -398,10 +442,11 @@ Job.prototype = {
     if (err) {
       if (err.type === 'exitCode') code = err.code;
       if ('number' === typeof err) code = err;
+      if ('exitCode' in err) code = err.exitCode;
 
       this.status('phase.errored', {
         time: now,
-        exitCode: code
+        exitCode: code,
       });
 
       if (code === 0) {
@@ -411,16 +456,22 @@ Job.prototype = {
     }
     // Complain if no plugins actually did anything during testing?
     // XXX: should this be a fail hard?
-    if (['deploy', 'test'].indexOf(this.phase) !== -1 && utils.sum(actuallyRan) === 0) {
-      this.out(`Phase ${this.phase} didn't actually run anything. Check your plugin configuration`, 'warn');
+    if (
+      ['deploy', 'test'].indexOf(this.phase) !== -1 &&
+      utils.sum(actuallyRan) === 0
+    ) {
+      this.out(
+        `Phase ${this.phase} didn't actually run anything. Check your plugin configuration`,
+        'warn'
+      );
     }
 
     if (this.phase === 'test') {
-      this.status('tested', {time: now, exitCode: code});
+      this.status('tested', { time: now, exitCode: code });
     }
 
     if (this.phase === 'deploy') {
-      this.status('deployed', {time: now, exitCode: code});
+      this.status('deployed', { time: now, exitCode: code });
     }
 
     var nextPhase = PHASES[PHASES.indexOf(this.phase) + 1];
@@ -436,7 +487,7 @@ Job.prototype = {
       time: now,
       exitCode: code,
       next: nextPhase,
-      elapsed: now.getTime() - started.getTime()
+      elapsed: now.getTime() - started.getTime(),
     });
 
     if (this.phase === 'cleanup') {
@@ -451,7 +502,10 @@ Job.prototype = {
     debug(`Job "${this.id}" errored:`, error.message, error.stack);
 
     if (!serverOnly) {
-      this.status('stderr', `${text.errorPleaseReport}\n\n${error.message}\n\n${error.stack}`);
+      this.status(
+        'stderr',
+        `${text.errorPleaseReport}\n\n${error.message}\n\n${error.stack}`
+      );
     }
   },
 
@@ -468,10 +522,11 @@ Job.prototype = {
   out: function (text, type) {
     debug(`Job "${this.id}" prints:`, type, text);
 
-    var dest = ['error', 'stderr', 'warn'].indexOf(type) !== -1 ? 'stderr' : 'stdout';
+    var dest =
+      ['error', 'stderr', 'warn'].indexOf(type) !== -1 ? 'stderr' : 'stdout';
 
-    text = (type && colorize[type]) ? colorize[type](text) : text;
+    text = type && colorize[type] ? colorize[type](text) : text;
 
     this.status(dest, text);
-  }
+  },
 };
