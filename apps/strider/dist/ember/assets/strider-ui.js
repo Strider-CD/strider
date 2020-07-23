0330cd7d430a7b2dd7ae212d1c457c53c81074ce
Ilya Radchenko <knownasilya@gmail.com>
2020-07-23 17:42:22 -0400

    Fix bad import (#1111)
    * fix bad import and build
    
    * changes

--- apps/strider/dist/ember/assets/strider-ui.js
+++ apps/strider/dist/ember/assets/strider-ui.js
@@ -2508,7 +2508,7 @@ catch(err) {
 
 ;
           if (!runningTests) {
-            require("strider-ui/app")["default"].create({"name":"strider-ui","version":"0.0.0+74f538e3"});
+            require("strider-ui/app")["default"].create({"name":"strider-ui","version":"0.0.0+41b25146"});
           }
         
 //# sourceMappingURL=strider-ui.map
